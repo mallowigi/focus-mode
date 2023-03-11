48a5c9b05eb36ce360da88dff8ea5f8e7412d204
Elior <heliosaian@gmail.com>
2023-03-11 18:26:30 +0200

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -191,7 +191,7 @@ tasks {
 
   publishPlugin {
     dependsOn("patchChangelog")
-    token.set(environment("PUBLISH_TOKEN"))
+    token.set(System.getenv("INTELLIJ_PUBLISH_TOKEN") ?: file("./publishToken").readText().trim())
     // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
     // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
     // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
